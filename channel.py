8f65fdbe1c2c81866336e46692eb03316f20d507
Hodwink <daniilgolosov232@gmail.com>
2025-03-30 12:33:54 +0300

    Update channel.py

--- channel.py
+++ channel.py
@@ -1,37 +1,39 @@
-from dotenv import find_dotenv, load_dotenv
-import os 
 import asyncio
-from loguru import logger
+import requests
+from bs4 import BeautifulSoup
+from random import choice
 from aiogram import Bot, Dispatcher, types, F
-from aiogram.client.default import DefaultBotProperties
-from private_chat import setup_private_handlers
-from group_chat import setup_channel_handlers
-from channel import setup_channel_handlers
-
+from aiogram.filters import Command
+from loguru import logger
+from dotenv import load_dotenv, find_dotenv
+import os
 
 load_dotenv(find_dotenv())
-CHANEL_ID = os.getenv("CHANEL_ID")
+CHANNEL_ID = os.getenv("CHANNEL_ID")
 
 
 async def send_jokes_task(bot: Bot):
     while True:
         try:
-            response = requests.get("https://www.anekdot.ru/random/anekbot/")
+            response = requests.get("https://www.anekdot.ru/random/anekdot/")
             if response.ok:
-                soup = BeautifulSoup(response.txt, 'html.parser')
-                jokes = soup.find_all('div', class_="text")
+                soup = BeautifulSoup(response.text, 'html.parser')
+                jokes = soup.find_all('div', class_='text')
                 joke = choice(jokes).text.strip()
-                await bot.send_message(CHANEL_ID, f"Анекдот:\n{joke}")
+                await bot.send_message(CHANNEL_ID, f"Анекдот:\n{joke}")
                 logger.success("Канал: анекдот отправлен")
             else:
                 logger.warning("Канал: проблема с сайтом анекдотов")
         except Exception as e:
             logger.error(f"Канал: ошибка {e}")
 
+        await asyncio.sleep(30)
+
+
 def setup_channel_handlers(dp: Dispatcher, bot: Bot):
     asyncio.create_task(send_jokes_task(bot))
 
-@dp.message(Command('chanel_stats'), F.chat.type == 'channel')
-async def channel_stats(message: types.Message):
-    await message.answer("бот канала работает")
-    logger.info("бот канала создан и работает") 
+    @dp.message(Command('channel_stats'), F.chat.type == "channel")
+    async def channel_stats(message: types.Message):
+        await message.answer("Бот канала работает!")
+        logger.info("Канал: проверка работы")
